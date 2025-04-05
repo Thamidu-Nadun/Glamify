93e9cc22a3f4d4373cddd87aee2ada729e942618
Thamidu-Nadun <nadunrz101@gmail.com>
2025-04-05 20:42:30 +0530

    Update client code: Add .prettierrc.json, update Navbar component

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
   return (
-    <div className="flex w-screen items-center">
-        <h2 className="bg-red-800 font-semibold font-monospace text-2xl">Glamify</h2>
-        <ol className='bg-green-700 flex'>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Book An Appointment</li>
-            <li>Testimonials</li>
-        </ol>
-        <div className="action-btn flex justify-content-center gap-2">
-            <button className="border-2 border-amber-500 rounded-lg px-3 py-2">Log in</button>
-            <button className="border-2 border-gray-500 rounded-lg px-3 py-2">Sign up</button>
+    <>
+      <div className="flex justify-between items-center w-full m-2 font-monospace">
+        <div className="text-2xl font-medium">
+          <h1>Glamify</h1>
         </div>
-    </div>
-  )
+        <div className="nav-links">
+          <ul className="flex">
+            <Link name="Home" />
+            <Link name="About" />
+            <Link name="Services" />
+            <Link name="Contact" />
+          </ul>
+        </div>
+        <div className="auth-buttons">
+          <LoginButton />
+          <SignUpButton />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+const LoginButton = () => {
+  return (
+    <>
+      <button className="border-2 border-amber-400 py-2 px-5 rounded-3xl hover:bg-amber-400 hover:border-transparent transition-colors duration-200 mx-2 text-amber-400 hover:text-amber-50 font-bold">
+        Login
+      </button>
+    </>
+  );
+};
+
+const SignUpButton = () => {
+  return (
+    <>
+      <button className="bg-black border-2 py-2 px-5 rounded-3xl hover:bg-amber-100 transition-colors duration-200 mx-2 text-white gray-900 hover:text-black font-bold">
+        Sign Up
+      </button>
+    </>
+  );
+};
+
+const Link = ({ name = 'Link' }) => {
+  return (
+    <li className="m-1 border-2 border-transparent px-4 py-2 rounded-2xl bg-gray-200/30 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 shadow-sm hover:bg-gray-200/50 hover:border-2 hover:border-gray-200 transition-colors">
+      {name}
+    </li>
+  );
+};
