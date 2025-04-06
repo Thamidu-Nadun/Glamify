b31ae3814b271f35a496d9590f151380b4448d9b
Thamidu-Nadun <nadunrz101@gmail.com>
2025-04-06 21:04:54 +0530

    created Hero Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
 
 function App() {
   return (
     <div>
       {/* <h2 className="underline text-green-600">Hello World!</h2> */}
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Hero />
     </div>
   )
 }
---
+++ client/src/Components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+function Hero() {
+  return (
+      <>
+          <Navbar />
+          <div className="hero">
+              
+          </div>
+    </>
+  )
+}
+
+export default Hero
