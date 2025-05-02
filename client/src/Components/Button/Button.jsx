f074e3f86e9c847de1fdd2dbfad50c9486ab1b03
GitHub Action <action@github.com>
2025-05-02 17:53:04 +0000

    chore: auto-format with Prettier

--- client/src/Components/Button/Button.jsx
+++ client/src/Components/Button/Button.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 function Button(props) {
-    const { title, onClick, type, disabled, isFullWidth, bgColor, color } = props;
+  const { title, onClick, type, disabled, isFullWidth, bgColor, color } = props;
   return (
-      <>
-          <button className={`mx-2 my-3 px-4 py-3 rounded-2xl font-bold font-mono ${isFullWidth ? 'w-full' : 'w-auto'} ${bgColor ? bgColor : 'bg-green-300'} ${color ? color : 'text-black'}`}>{title}</button>
+    <>
+      <button
+        className={`mx-2 my-3 rounded-2xl px-4 py-3 font-mono font-bold ${isFullWidth ? 'w-full' : 'w-auto'} ${bgColor ? bgColor : 'bg-green-300'} ${color ? color : 'text-black'}`}
+      >
+        {title}
+      </button>
     </>
-  )
+  );
 }
 
-export default Button
+export default Button;
