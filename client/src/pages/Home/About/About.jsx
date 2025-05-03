b80c61b7fc654cc22a11029e7a523a4e51722cf2
GitHub Action <action@github.com>
2025-05-03 18:54:36 +0000

    chore: auto-format with Prettier

--- client/src/pages/Home/About/About.jsx
+++ client/src/pages/Home/About/About.jsx
@@ -1,36 +1,45 @@
 import React from 'react';
-import { FaArrowRight } from "react-icons/fa6";
+import { FaArrowRight } from 'react-icons/fa6';
 import women_image from '../assets/Serene_Portrait_of_a_Young_Woman.jpg';
 import productImage from '../assets/product.jpg';
 import './About.css';
 
-function About () {
+function About() {
   return (
-    <div className="flex h-fit w-screen items-center justify-center p-[2rem] bg-white">
-      <div className="py-5 bg-[#f8dbfe] w-full h-full rounded-2xl flex flex-col lg:flex-row items-center lg:justify-start gap-5">
-        <div className="box-action w-[50%] flex flex-col justify-center items-center pl-[2rem] pr-[2rem] lg:items-start gap-2">
-        <div className="text-center lg:text-start">
-              <span className='border border-black bg-transparent px-2 py-1 rounded-xl text-center text-sm w-fit'>BENEFIT OF NATURAL INGREDIENTS</span>
-              <h3 className='text-4xl font-semibold font-sans'>
-                Benefit of natural ingredients
-              </h3>
+    <div className="flex h-fit w-screen items-center justify-center bg-white p-[2rem]">
+      <div className="flex h-full w-full flex-col items-center gap-5 rounded-2xl bg-[#f8dbfe] py-5 lg:flex-row lg:justify-start">
+        <div className="box-action flex w-[50%] flex-col items-center justify-center gap-2 pr-[2rem] pl-[2rem] lg:items-start">
+          <div className="text-center lg:text-start">
+            <span className="w-fit rounded-xl border border-black bg-transparent px-2 py-1 text-center text-sm">
+              BENEFIT OF NATURAL INGREDIENTS
+            </span>
+            <h3 className="font-sans text-4xl font-semibold">
+              Benefit of natural ingredients
+            </h3>
           </div>
-          <p className='text-lg font-normal text-black/50 text-center lg:text-start'>
-            Unlock skin effortlessly by harnessing the potent benefits of natural ingredients for a healthy, glowing complexion.
+          <p className="text-center text-lg font-normal text-black/50 lg:text-start">
+            Unlock skin effortlessly by harnessing the potent benefits of
+            natural ingredients for a healthy, glowing complexion.
           </p>
-          <button className="mt-3 w-fit bg-black text-white text-lg px-3 py-2 rounded-xl flex gap-1 items-center">
-            Let's explore <FaArrowRight className='rotate-[-45deg]'/>
+          <button className="mt-3 flex w-fit items-center gap-1 rounded-xl bg-black px-3 py-2 text-lg text-white">
+            Let's explore <FaArrowRight className="rotate-[-45deg]" />
           </button>
         </div>
-        <div className="relative box-image w-[50%] flex flex-col justify-center items-center">
+        <div className="box-image relative flex w-[50%] flex-col items-center justify-center">
           <div className="seller invisible lg:visible">
             <span>Best Seller</span>
             <img src={productImage} alt="A good product" />
           </div>
-          <img src={women_image} alt="Serene_Portrait_of_a_Young_Woman" className='w-50 rounded-2xl' />
-          <div className="sm-box w-40 border border-white bg-[#fe8158] rounded-2xl font-semibold p-2">
+          <img
+            src={women_image}
+            alt="Serene_Portrait_of_a_Young_Woman"
+            className="w-50 rounded-2xl"
+          />
+          <div className="sm-box w-40 rounded-2xl border border-white bg-[#fe8158] p-2 font-semibold">
             <div className="percentage text-lg">100%</div>
-            <span className='text-sm text-black/80'>Product original and BPOM.</span>
+            <span className="text-sm text-black/80">
+              Product original and BPOM.
+            </span>
           </div>
         </div>
       </div>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Hero from './Hero/Hero'
-import Appointment from './Appointment/Appointment'
-import About from './About/About'
-import AboutS from './About/AboutS'
+import React from 'react';
+import Hero from './Hero/Hero';
+import Appointment from './Appointment/Appointment';
+import About from './About/About';
+import AboutS from './About/AboutS';
 
 function Home() {
   return (
@@ -12,7 +12,7 @@ function Home() {
       <About />
       <AboutS />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
