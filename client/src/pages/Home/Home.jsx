50890fec225ac1333b5da9df6af47494056096be
Thamidu-Nadun <nadunrz101@gmail.com>
2025-05-04 00:22:09 +0530

    Remove AboutS.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from './Hero/Hero';
 import Appointment from './Appointment/Appointment';
 import About from './About/About';
-import AboutS from './About/AboutS';
 
 function Home() {
   return (
@@ -10,7 +9,6 @@ function Home() {
       <Hero />
       <Appointment />
       <About />
-      <AboutS />
     </>
   );
 }
