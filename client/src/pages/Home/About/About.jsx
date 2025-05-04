55f648d139c6da692218ec51c3e81370760cc1de
Thamidu-Nadun <nadunrz101@gmail.com>
2025-05-04 23:54:31 +0530

    Refactor About page layout and styling

--- client/src/pages/Home/About/About.jsx
+++ client/src/pages/Home/About/About.jsx
@@ -25,21 +25,17 @@ function About() {
             Let's explore <FaArrowRight className="rotate-[-45deg]" />
           </button>
         </div>
-        <div className="box-image relative flex w-[50%] flex-col items-center justify-center">
-          <div className="seller invisible lg:visible">
-            <span>Best Seller</span>
-            <img src={productImage} alt="A good product" />
-          </div>
-          <img
-            src={women_image}
-            alt="Serene_Portrait_of_a_Young_Woman"
-            className="w-50 rounded-2xl"
-          />
-          <div className="sm-box w-40 rounded-2xl border border-white bg-[#fe8158] p-2 font-semibold">
-            <div className="percentage text-lg">100%</div>
-            <span className="text-sm text-black/80">
-              Product original and BPOM.
-            </span>
+        <div className="box-image relative flex w-[50%] flex-col items-center justify-center bg-black">
+          <div className="img-box">
+            <img src={women_image} className='w-full h-full rounded-3xl' />
+            <div className="seller w-[10vw] h-25 relative">
+              <img src={productImage} className="w-full h-full rounded-3xl" />
+              <span className='absolute top-0 left-0 text-white text-xl font-bold m-2'>Best Seller</span>
+            </div>
+            <div className="percentage w-[10vw] h-25 bg-[#fe8158] flex flex-col items-center relative rounded-2xl">
+              <div className="percentage text-lg top-0 left-0 p-4">100%</div>
+              <span className="text-sm absolute text-black/70 bottom-0 right-0 p-2 text-right">Product original and BPOM.</span>
+            </div>
           </div>
         </div>
       </div>
