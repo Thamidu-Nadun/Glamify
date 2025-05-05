b741e8fac2d025f1d944a1aac976d5ff1dd76ede
GitHub Action <action@github.com>
2025-05-05 18:33:07 +0000

    chore: auto-format with Prettier

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import NavBar from '../../layout/NavBar/NavBar'
-import Footer from '../../layout/Footer/Footer'
-import Sidebar from './components/SideBar/Sidebar'
+import React from 'react';
+import NavBar from '../../layout/NavBar/NavBar';
+import Footer from '../../layout/Footer/Footer';
+import Sidebar from './components/SideBar/Sidebar';
 
 function Admin() {
   return (
-      <>
-          <div className="bar w-screen h-fit py-3 bg-[#fe8158] text-center">
-              <span className="text-lg text-white font-bold">Glamify</span>
-          </div>
-          <Sidebar />
-          <div className="board w-screen h-screen bg-black"></div>
-          <Footer />
+    <>
+      <div className="bar h-fit w-screen bg-[#fe8158] py-3 text-center">
+        <span className="text-lg font-bold text-white">Glamify</span>
+      </div>
+      <Sidebar />
+      <div className="board h-screen w-screen bg-black"></div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
