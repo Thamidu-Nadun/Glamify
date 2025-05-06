8b7aa62fe3e496f437e321b8889bbd8c56b13a60
Thamidu-Nadun <nadunrz101@gmail.com>
2025-05-06 18:10:58 +0530

    Add new files and make changes to existing files

--- server/app/src/main/java/com/glamify/app/controller/AdminController.java
+++ server/app/src/main/java/com/glamify/app/controller/AdminController.java
@@ -1,4 +1,17 @@
 package com.glamify.app.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/admin")
+@CrossOrigin
 public class AdminController {
+
+    @GetMapping("/getAdmin")
+    public String getAdmin() {
+        return "Admin";
+    }
 }
---
+++ server/app/src/main/java/com/glamify/app/entity/Admin.java
@@ -0,0 +1,4 @@
+package com.glamify.app.entity;
+
+public class Admin {
+}
