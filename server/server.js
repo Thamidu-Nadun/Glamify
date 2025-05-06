842bf991065820b0fcf8de5d322c6ef5b50945b5
Hirula <hirulapinibinda01@gmail.com>
2025-05-06 15:32:49 +0530

    edited server.js

--- server/server.js
+++ server/server.js
@@ -6,4 +6,4 @@ app.use(express.json());
 app.use('/api/customers', customerRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));q
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
